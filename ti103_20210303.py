6777dd7983a08c6b50c410c43ad94e17b781e93d
Guillaume Mantelet <guillaume_mantelet@hotmail.com>
2021-03-03 16:32:18 -0600

    A l'issue du cours du 3 Mars

--- ti103_20210303.py
+++ ti103_20210303.py
@@ -1,2 +1,42 @@
+import random
 
 
+class Etudiant:
+    """
+    +==============+
+    | Etudiant     |
+    +==============+
+    | nom          |
+    | prenom       |
+    | numero       |
+    | adresse      |
+    +==============+
+    """
+    id_ = 0
+
+    def __init__(self, nom, prenom, adresse):
+        self.nom = nom
+        self.prenom = prenom
+        self.numero = self.__class__.id_
+        self.adresse = adresse
+        self.x = random.randint(1, 15)
+
+        self.__class__.id_ += 1
+
+    def email(self):
+        return self.prenom + "." + self.nom + "@usb.ca"
+
+
+jean_dupont = Etudiant("dupont", "jean", "N/A")
+jeanne_dupont = Etudiant("dupont", "jeanne", "N/A")
+guillaume_mantelet = Etudiant("mantelet", "guillaume", "N/A")
+
+print(jean_dupont.nom)
+print(guillaume_mantelet.nom)
+print(jean_dupont.email())
+
+print(Etudiant.id_)
+
+print(jean_dupont)
+print(id(jean_dupont))
+print(id(Etudiant))
